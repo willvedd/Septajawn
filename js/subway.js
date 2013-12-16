547dde64894b30aa7b6e0ce19cc1a9ac925f78af
Will V <willvedd@gmail.com>
2013-12-16 15:07:04 -0500

    Improved integration of lines. All non-transfer routing works. Routing that requires transfers needs work.

--- js/subway.js
+++ js/subway.js
@@ -221,16 +221,18 @@ var att = {
 	sched_sun: schedule(oregon.sched_wk_sb,3),
 	sched_wk_nb: schedule(oregon.sched_wk_sb,-3),
 };
-//Market Frankford
+//Market Frankford Line
 var frank = {
 	line: "mf",
 	order: 1,
 	id:   "frank",
 	name: "Frankford",
-	sched_wk_sb: [1,2,3],
-	sched_sat: [1,2,3],
-	sched_sun: [1,2,3],
-	sched_wk_nb: [1,2,3],
+	sched_wk_sb: [5.0,5.14,5.27,5.39,5.41,5.5,6.0,6.09,6.17,6.24,6.3,6.35,6.4,6.44,6.48,6.52,6.56,7.0,7.08,7.16,7.24,7.32,7.4,7.48,7.56,8.04,8.12,8.20,8.28,8.36,8.44,8.52,9,9.04,9.08,9.12,9.17,9.23,9.29,9.35,9.41,9.47,9.53,9.59,10.05,10.11,10.17,10.23,10.29,10.35,10.41,10.47,10.53,10.59,11.05,11.11,11.17,11.23,11.29,11.35,11.41,11.47,11.53,11.59,12.05,12.11,12.17,12.23,12.29,12.35,12.41,12.47,12.53,12.59,13.05,13.11,13.17,13.23,13.29,13.35,13.40,13.45,13.50,13.56,14.0,14.04,14.09,14.14,14.19,14.24,14.29,14.34,14.39,14.44,14.49,14.54,14.59,15.04,15.09,15.14,15.19,15.24,15.29,15.32,15.36,15.40,15.44,15.48,15.52,15.56,16.00,16.04,16.08,16.12,16.16,16.20,16.24,16.28,16.32,16.36,16.40,16.44,16.48,16.52,16.56,17.00,17.04,17.08,17.12,17.16,17.20,17.24,17.28,17.32,17.36,17.40,17.45,17.50,17.55,18.00,18.05,18.11,18.18,18.26,18.35,18.45,18.55,19.05,19.15,19.25,19.35,19.45,19.55,20.05,20.15,20.25,20.35,20.45,20.56,21.07,21.19,21.31,21.43,21.55,22.07,22.19,22.31,22.43,22.55,23.08,23.21,23.36,23.51,24.01],
+	sched_sat_sb: [1,2,3],
+	sched_sun_sb: [1,2,3],
+	sched_wk_nb: [5.4,5.54,6.06,6.16,6.25,6.33,6.39,6.45,6.51,6.57,7.03,7.09,7.14,7.19,7.23,7.27,7.31,7.35,7.42,7.50,7.58,8.06,8.14,8.22,8.30,8.38,8.46,8.54,9.02,9.10,9.18,9.26,9.34,9.39,9.44,9.49,9.55,10.01,10.07,10.13,10.19,10.25,10.31,10.37,10.43,10.49,10.55,11.01,11.07,11.13,11.19,11.25,11.31,11.37,11.43,11.49,11.55,12.01,12.07,12.13,12.19,12.25,12.34,12.37,12.43,12.49,12.55,13.01,13.07,13.13,13.19,13.25,13.31,13.37,13.43,13.49,13.55,14.01,14.06,14.12,14.17,14.22,14.27,14.32,14.37,14.42,14.47,14.52,14.57,15.02,15.07,15.12,15.17,15.22,15.27,15.33,15.38,15.42,15.46,15.50,15.54,15.58,16.02,16.06,16.10,16.14,16.18,16.22,16.26,16.30,16.34,16.42,16.50,16.58,17.06,17.14,17.22,17.30,17.38,17.46,17.54,18.02,18.10,18.15,18.19,18.24,18.29,18.35,18.42,18.50,18.59,19.05,19.09,19.19,19.29,19.39,19.49,19.59,20.09,20.19,20.29,20.39,20.49,21.01,21.13,21.25,21.37,21.49,22.01,22.13,22.25,22.37,22.49,23.01,23.13,23.25,23.39,23.54,24.09,24.24,24.39,24.54],
+	sched_sat_nb: [1,2,3],
+	sched_sun_nb: [1,2,3],
 };
 var marg = {
 	line: "mf",
@@ -240,7 +242,7 @@ var marg = {
 	sched_wk_sb: schedule(frank.sched_wk_sb,2),
 	sched_sat: schedule(frank.sched_wk_sb,2),
 	sched_sun: schedule(frank.sched_wk_sb,2),
-	sched_wk_nb: schedule(frank.sched_wk_sb,-2),
+	sched_wk_nb: schedule(frank.sched_wk_nb,-2),
 };
 var church = {
 	line: "mf",
@@ -250,7 +252,7 @@ var church = {
 	sched_wk_sb: schedule(marg.sched_wk_sb,1),
 	sched_sat: schedule(marg.sched_wk_sb,1),
 	sched_sun: schedule(marg.sched_wk_sb,1),
-	sched_wk_nb: schedule(marg.sched_wk_sb,-1),
+	sched_wk_nb: schedule(marg.sched_wk_nb,-1),
 };
 var eriet = {
 	line: "mf",
@@ -260,7 +262,7 @@ var eriet = {
 	sched_wk_sb: schedule(church.sched_wk_sb,2),
 	sched_sat: schedule(church.sched_wk_sb,2),
 	sched_sun: schedule(church.sched_wk_sb,2),
-	sched_wk_nb: schedule(church.sched_wk_sb,-2),
+	sched_wk_nb: schedule(church.sched_wk_nb,-2),
 };
 var tioga = {
 	line: "mf",
@@ -270,7 +272,7 @@ var tioga = {
 	sched_wk_sb: schedule(eriet.sched_wk_sb,1),
 	sched_sat: schedule(eriet.sched_wk_sb,1),
 	sched_sun: schedule(eriet.sched_wk_sb,1),
-	sched_wk_nb: schedule(eriet.sched_wk_sb,-1),
+	sched_wk_nb: schedule(eriet.sched_wk_nb,-1),
 };
 var alleghmf = {
 	line: "mf",
@@ -280,7 +282,7 @@ var alleghmf = {
 	sched_wk_sb: schedule(tioga.sched_wk_sb,1),
 	sched_sat: schedule(tioga.sched_wk_sb,1),
 	sched_sun: schedule(tioga.sched_wk_sb,1),
-	sched_wk_nb: schedule(tioga.sched_wk_sb,-1),
+	sched_wk_nb: schedule(tioga.sched_wk_nb,-1),
 };
 var somer = {
 	line: "mf",
@@ -290,7 +292,7 @@ var somer = {
 	sched_wk_sb: schedule(alleghmf.sched_wk_sb,2),
 	sched_sat: schedule(alleghmf.sched_wk_sb,2),
 	sched_sun: schedule(alleghmf.sched_wk_sb,2),
-	sched_wk_nb: schedule(alleghmf.sched_wk_sb,-2),
+	sched_wk_nb: schedule(alleghmf.sched_wk_nb,-2),
 };
 var hunt = {
 	line: "mf",
@@ -300,7 +302,7 @@ var hunt = {
 	sched_wk_sb: schedule(somer.sched_wk_sb,1),
 	sched_sat: schedule(somer.sched_wk_sb,1),
 	sched_sun: schedule(somer.sched_wk_sb,1),
-	sched_wk_nb: schedule(somer.sched_wk_sb,-1),
+	sched_wk_nb: schedule(somer.sched_wk_nb,-1),
 };
 var york = {
 	line: "mf",
@@ -310,7 +312,7 @@ var york = {
 	sched_wk_sb: schedule(hunt.sched_wk_sb,1),
 	sched_sat: schedule(hunt.sched_wk_sb,1),
 	sched_sun: schedule(hunt.sched_wk_sb,1),
-	sched_wk_nb: schedule(hunt.sched_wk_sb,-1),
+	sched_wk_nb: schedule(hunt.sched_wk_nb,-1),
 };
 var berks = {
 	line: "mf",
@@ -320,7 +322,7 @@ var berks = {
 	sched_wk_sb: schedule(york.sched_wk_sb,2),
 	sched_sat: schedule(york.sched_wk_sb,2),
 	sched_sun: schedule(york.sched_wk_sb,2),
-	sched_wk_nb: schedule(york.sched_wk_sb,-2),
+	sched_wk_nb: schedule(york.sched_wk_nb,-2),
 };
 var girardmf = {
 	line: "mf",
@@ -330,7 +332,7 @@ var girardmf = {
 	sched_wk_sb: schedule(berks.sched_wk_sb,2),
 	sched_sat: schedule(berks.sched_wk_sb,2),
 	sched_sun: schedule(berks.sched_wk_sb,2),
-	sched_wk_nb: schedule(berks.sched_wk_sb,-2),
+	sched_wk_nb: schedule(berks.sched_wk_nb,-2),
 };
 var springmf = {
 	line: "mf",
@@ -340,7 +342,7 @@ var springmf = {
 	sched_wk_sb: schedule(girardmf.sched_wk_sb,2),
 	sched_sat: schedule(girardmf.sched_wk_sb,2),
 	sched_sun: schedule(girardmf.sched_wk_sb,2),
-	sched_wk_nb: schedule(girardmf.sched_wk_sb,-2),
+	sched_wk_nb: schedule(girardmf.sched_wk_nb,-2),
 };
 var second = {
 	line: "mf",
@@ -350,7 +352,7 @@ var second = {
 	sched_wk_sb: schedule(springmf.sched_wk_sb,2),
 	sched_sat: schedule(springmf.sched_wk_sb,2),
 	sched_sun: schedule(springmf.sched_wk_sb,2),
-	sched_wk_nb: schedule(springmf.sched_wk_sb,-2),
+	sched_wk_nb: schedule(springmf.sched_wk_nb,-2),
 };
 var fifth = {
 	line: "mf",
@@ -360,7 +362,7 @@ var fifth = {
 	sched_wk_sb: schedule(second.sched_wk_sb,1),
 	sched_sat: schedule(second.sched_wk_sb,1),
 	sched_sun: schedule(second.sched_wk_sb,1),
-	sched_wk_nb: schedule(second.sched_wk_sb,-1),
+	sched_wk_nb: schedule(second.sched_wk_nb,-1),
 };
 var eight = {
 	line: "mf",
@@ -370,7 +372,7 @@ var eight = {
 	sched_wk_sb: schedule(fifth.sched_wk_sb,1),
 	sched_sat: schedule(fifth.sched_wk_sb,1),
 	sched_sun: schedule(fifth.sched_wk_sb,1),
-	sched_wk_nb: schedule(fifth.sched_wk_sb,-1),
+	sched_wk_nb: schedule(fifth.sched_wk_nb,-1),
 };
 var elev = {
 	line: "mf",
@@ -380,122 +382,132 @@ var elev = {
 	sched_wk_sb: schedule(eight.sched_wk_sb,1),
 	sched_sat: schedule(eight.sched_wk_sb,1),
 	sched_sun: schedule(eight.sched_wk_sb,1),
-	sched_wk_nb: schedule(eight.sched_wk_sb,-1),
+	sched_wk_nb: schedule(eight.sched_wk_nb,-1),
 };
-var fif = {
+var thirteen = {
 	line: "mf",
 	order: 17,
-	id:   "fif",
-	name: "15h St",
+	id:   "thirteen",
+	name: "13th St",
 	sched_wk_sb: schedule(elev.sched_wk_sb,1),
 	sched_sat: schedule(elev.sched_wk_sb,1),
 	sched_sun: schedule(elev.sched_wk_sb,1),
-	sched_wk_nb: schedule(elev.sched_wk_sb,-1),
+	sched_wk_nb: schedule(elev.sched_wk_nb,-1),
+};
+var fif = {
+	line: "mf",
+	order: 18,
+	id:   "fif",
+	name: "15th St",
+	sched_wk_sb: schedule(thirteen.sched_wk_sb,1),
+	sched_sat: schedule(thirteen.sched_wk_sb,1),
+	sched_sun: schedule(thirteen.sched_wk_sb,1),
+	sched_wk_nb: schedule(thirteen.sched_wk_nb,-1),
 };
 var thirty = {
 	line: "mf",
-	order: 18,
+	order: 19,
 	id:   "thirty",
 	name: "30th St",
-	sched_wk_sb: schedule(fifth.sched_wk_sb,2),
-	sched_sat: schedule(fifth.sched_wk_sb,2),
-	sched_sun: schedule(fifth.sched_wk_sb,2),
-	sched_wk_nb: schedule(fifth.sched_wk_sb,-2),
+	sched_wk_sb: schedule(fif.sched_wk_sb,2),
+	sched_sat: schedule(fif.sched_wk_sb,2),
+	sched_sun: schedule(fif.sched_wk_sb,2),
+	sched_wk_nb: schedule(fif.sched_wk_nb,-2),
 };
 var thirtyfour = {
 	line: "mf",
-	order: 19,
+	order: 20,
 	id:   "thirtyfour",
 	name: "34th St",
 	sched_wk_sb: schedule(thirty.sched_wk_sb,1),
 	sched_sat: schedule(thirty.sched_wk_sb,1),
 	sched_sun: schedule(thirty.sched_wk_sb,1),
-	sched_wk_nb: schedule(thirty.sched_wk_sb,-1),
+	sched_wk_nb: schedule(thirty.sched_wk_nb,-1),
 };
 var fourty = {
 	line: "mf",
-	order: 20,
+	order: 21,
 	id:   "forty",
 	name: "40th St",
 	sched_wk_sb: schedule(thirtyfour.sched_wk_sb,2),
 	sched_sat: schedule(thirtyfour.sched_wk_sb,2),
 	sched_sun: schedule(thirtyfour.sched_wk_sb,2),
-	sched_wk_nb: schedule(thirtyfour.sched_wk_sb,-2),
+	sched_wk_nb: schedule(thirtyfour.sched_wk_nb,-2),
 };
 var fourtysix = {
 	line: "mf",
-	order: 21,
+	order: 22,
 	id:   "fourtysix",
 	name: "46th St",
 	sched_wk_sb: schedule(fourty.sched_wk_sb,2),
 	sched_sat: schedule(fourty.sched_wk_sb,2),
 	sched_sun: schedule(fourty.sched_wk_sb,2),
-	sched_wk_nb: schedule(fourty.sched_wk_sb,-2),
+	sched_wk_nb: schedule(fourty.sched_wk_nb,-2),
 };
 var fiftytwo = {
 	line: "mf",
-	order: 22,
+	order: 23,
 	id:   "fiftytwo",
 	name: "52nd St",
 	sched_wk_sb: schedule(fourtysix.sched_wk_sb,1),
 	sched_sat: schedule(fourtysix.sched_wk_sb,1),
 	sched_sun: schedule(fourtysix.sched_wk_sb,1),
-	sched_wk_nb: schedule(fourtysix.sched_wk_sb,-1),
+	sched_wk_nb: schedule(fourtysix.sched_wk_nb,-1),
 };
 var fiftysix = {
 	line: "mf",
-	order: 23,
+	order: 24,
 	id:   "fiftytwo",
 	name: "52nd St",
 	sched_wk_sb: schedule(fiftytwo.sched_wk_sb,1),
 	sched_sat: schedule(fiftytwo.sched_wk_sb,1),
 	sched_sun: schedule(fiftytwo.sched_wk_sb,1),
-	sched_wk_nb: schedule(fiftytwo.sched_wk_sb,-1),
+	sched_wk_nb: schedule(fiftytwo.sched_wk_nb,-1),
 };
 var sixty = {
 	line: "mf",
-	order: 24,
+	order: 25,
 	id:   "sixty",
 	name: "60th St",
 	sched_wk_sb: schedule(fiftysix.sched_wk_sb,2),
 	sched_sat: schedule(fiftysix.sched_wk_sb,2),
 	sched_sun: schedule(fiftysix.sched_wk_sb,2),
-	sched_wk_nb: schedule(fiftysix.sched_wk_sb,-2),
+	sched_wk_nb: schedule(fiftysix.sched_wk_nb,-2),
 };
 var sixtythree = {
 	line: "mf",
-	order: 25,
+	order: 26,
 	id:   "sixtythree",
 	name: "63rd St",
 	sched_wk_sb: schedule(sixty.sched_wk_sb,1),
 	sched_sat: schedule(sixty.sched_wk_sb,1),
 	sched_sun: schedule(sixty.sched_wk_sb,1),
-	sched_wk_nb: schedule(sixty.sched_wk_sb,-1),
+	sched_wk_nb: schedule(sixty.sched_wk_nb,-1),
 };
 var milb = {
 	line: "mf",
-	order: 26,
+	order: 27,
 	id:   "milb",
 	name: "Milbourne",
 	sched_wk_sb: schedule(sixtythree.sched_wk_sb,2),
 	sched_sat: schedule(sixtythree.sched_wk_sb,2),
 	sched_sun: schedule(sixtythree.sched_wk_sb,2),
-	sched_wk_nb: schedule(sixtythree.sched_wk_sb,-2),
+	sched_wk_nb: schedule(sixtythree.sched_wk_nb,-2),
 };
 var sixtynine = {
 	line: "mf",
-	order: 27,
+	order: 28,
 	id:   "sixtynine",
 	name: "69th St",
 	sched_wk_sb: schedule(milb.sched_wk_sb,4),
 	sched_sat: schedule(milb.sched_wk_sb,4),
 	sched_sun: schedule(milb.sched_wk_sb,4),
-	sched_wk_nb: schedule(milb.sched_wk_sb,-4),
+	sched_wk_nb: schedule(milb.sched_wk_nb,-4),
 };
 
 //-----------------------------------------------------------
 
-var stations = [fern,olney,logan,wyoming,hunt_park,erie,allegh,nphilly,susque,cecil,girard,fairmount,spring,race,city_hall,walnut,lombard,ellsworth,tasker,snyder,oregon,att,frank,marg,church,eriet,tioga,alleghmf,somer,hunt,york,berks,girardmf,springmf,second,fifth,eight,elev,fif,thirty,thirtyfour,fourty,fourtysix,fiftytwo,fiftysix,sixty,sixtythree,milb,sixtynine];//Setting array of stations
+var stations = [fern,olney,logan,wyoming,hunt_park,erie,allegh,nphilly,susque,cecil,girard,fairmount,spring,race,city_hall,walnut,lombard,ellsworth,tasker,snyder,oregon,att,frank,marg,church,eriet,tioga,alleghmf,somer,hunt,york,berks,girardmf,springmf,second,fifth,eight,elev,thirteen,fif,thirty,thirtyfour,fourty,fourtysix,fiftytwo,fiftysix,sixty,sixtythree,milb,sixtynine];//Setting array of stations
 
 //-----------------------------------------------------------
 
@@ -505,13 +517,11 @@ function submit(){
     var start_sel = document.getElementById("start_dest");
     var start = start_sel.options[start_sel.selectedIndex].value;
     
-    console.log("star_sel"+start_sel);
-    console.log("start"+start);
-    
     var end_sel = document.getElementById("end_dest");
     var end = end_sel.options[end_sel.selectedIndex].value;
         
-    route(start,end,parseTime(time),parseDay());
+    routeInit(start,end,parseTime(time),parseDay());
+
 };
 
 //-----------------------------------------------------------
@@ -584,11 +594,7 @@ function timeformat(time){//formats time from a double into hh:mm
 	
 	time = time.toString();
 	
-	console.log("Time: "+time);
-	
 	var mins = Math.floor(time);
-	
-	console.log("Mins: "+mins);
 	
 	var time2 = time.split(".");
 	
@@ -624,7 +630,7 @@ function parseDay(){
 
 //-----------------------------------------------------------
 
-function route(start,end,time,day){ //Using form to get times
+function routeInit(start,end,time,day){ //Using form to get times
 	for(i=0; i < stations.length; i++){
 		if(stations[i].id == start){
 			console.log("Starting from: "+stations[i].name);
@@ -634,10 +640,24 @@ function route(start,end,time,day){ //Using form to get times
 			console.log("Ending at: "+stations[i].name);
 			var end_station = stations[i];
 		};
-	}
+	};
+	
 	if(start_station.line != end_station.line){
-		console.log("Transfer Required!");
+		route3(start_station,end_station,day,time);
 	}
+	else{
+		route2(start_station,end_station,day,time);
+	};
+};
+
+//-----------------------------------------------------------
+
+function route2(start_station,end_station,day,time){
+	
+	$('.transfer_station').remove();
+	$('.transfer1').remove();
+	$('.transfer2').remove();
+	$('.transfer3').remove();
 	
 	if(start_station.order < end_station.order){//southbound
 		if(day === "wk"){//weekday southbound scheduling
@@ -723,6 +743,123 @@ function route(start,end,time,day){ //Using form to get times
 		};				
 	};
 	
+	render2(start_station,end_station,leave_time1,leave_time2,leave_time3,arrive_time1,arrive_time2,arrive_time3);	
+	
+};
+
+//-----------------------------------------------------------
+
+function route3(start_station,end_station,day,time){
+	
+	
+	if(start_station.line == "bs"){
+		var transfer_station = city_hall.name;
+	}
+	else{
+		var transfer_station = fif.name;
+	};
+	
+	
+	if(start_station.order < city_hall.order){//southbound & westbound
+		if(day === "wk"){//weekday southbound scheduling
+			if(start_station.line == "bs"){
+				var transfer_time1 = city_hall.sched_wk_sb[i];
+				var transfer_time2 = city_hall.sched_wk_sb[i+1];
+				var transfer_time3 = city_hall.sched_wk_sb[i+2];
+			}
+			else{
+				var transfer_time1 = fif.sched_wk_sb[i];
+				var transfer_time2 = fif.sched_wk_sb[i+1];
+				var transfer_time3 = fif.sched_wk_sb[i+2];
+			}; 				
+		}//endif
+		else if(day === "sat"){//saturday southbound & westbound scheduling
+			if(start_station.line == "bs"){
+				var transfer_time1 = city_hall.sched_sat_sb[i];
+				var transfer_time2 = city_hall.sched_sat_sb[i+1];
+				var transfer_time3 = city_hall.sched_sat_sb[i+2];
+			}
+			else{
+				var transfer_time1 = fif.sched_sat_sb[i];
+				var transfer_time2 = fif.sched_sat_sb[i+1];
+				var transfer_time3 = fif.sched_sat_sb[i+2];
+			};
+		}
+	else {//sunday southbound & westbound scheduling
+			if(start_station.line == "bs"){
+				var transfer_time1 = city_hall.sched_sun_sb[i];
+				var transfer_time2 = city_hall.sched_sun_sb[i+1];
+				var transfer_time3 = city_hall.sched_sun_sb[i+2];
+			}
+			else{
+				var transfer_time1 = fif.sched_sun_sb[i];
+				var transfer_time2 = fif.sched_sun_sb[i+1];
+				var transfer_time3 = fif.sched_sun_sb[i+2];
+			};	
+		};
+	}	
+	else{//northbound & eastbound
+		if(day === "wk"){//weekday northbound & eastbound scheduling
+			var leave_time1 = start_station.sched_wk_nb[i];
+			var leave_time2 = start_station.sched_wk_nb[i+1];
+			var leave_time3 = start_station.sched_wk_nb[i+2];
+			if(start_station.line == "bs"){
+				var transfer_time1 = city_hall.sched_wk_nb[i];
+				var transfer_time2 = city_hall.sched_wk_nb[i+1];
+				var transfer_time3 = city_hall.sched_wk_nb[i+2];
+			}
+			else{
+				var transfer_time1 = fif.sched_wk_nb[i];
+				var transfer_time2 = fif.sched_wk_nb[i+1];
+				var transfer_time3 = fif.sched_wk_nb[i+2];
+			};
+		}//endif
+		else if(day === "sat"){//saturday northbound & eastbound scheduling
+					
+			var leave_time1 = start_station.sched_sat_nb[i];
+			var leave_time2 = start_station.sched_sat_nb[i+1];
+			var leave_time3 = start_station.sched_sat_nb[i+2];
+			if(start_station.line == "bs"){
+				var transfer_time1 = city_hall.sched_sat_nb[i];
+				var transfer_time2 = city_hall.sched_sat_nb[i+1];
+				var transfer_time3 = city_hall.sched_sat_nb[i+2];
+			}
+			else{
+				var transfer_time1 = fif.sched_sat_nb[i];
+				var transfer_time2 = fif.sched_sat_nb[i+1];
+				var transfer_time3 = fif.sched_sat_nb[i+2];
+			};
+		}
+		else {//sunday northbound & eastbound scheduling
+			var leave_time1 = start_station.sched_sun_nb[i];
+			var leave_time2 = start_station.sched_sun_nb[i+1];
+			var leave_time3 = start_station.sched_sun_nb[i+2];
+			if(start_station.line == "bs"){
+				var transfer_time1 = city_hall.sched_sun_nb[i];
+				var transfer_time2 = city_hall.sched_sun_nb[i+1];
+				var transfer_time3 = city_hall.sched_sun_nb[i+2];
+			}
+			else{
+				var transfer_time1 = fif.sched_sun_nb[i];
+				var transfer_time2 = fif.sched_sun_nb[i+1];
+				var transfer_time3 = fif.sched_sun_nb[i+2];
+			};
+		};				
+	};
+	
+	var arrive_time1 = 12;
+	var arrive_time2 = 13;
+	var arrive_time3 = 14;
+	
+				render3(start_station,end_station,transfer_station,leave_time1,leave_time2,leave_time3,transfer_time1,transfer_time2,transfer_time3,arrive_time1,arrive_time2,arrive_time3);	
+	
+};
+
+
+//-----------------------------------------------------------
+
+function render2(start_station,end_station,leave_time1,leave_time2,leave_time3,arrive_time1,arrive_time2,arrive_time3){
+	
 	$('.start_station').empty();
 	$('.start_station').append(start_station.name);
 	$('.end_station').empty();
@@ -733,6 +870,38 @@ function route(start,end,time,day){ //Using form to get times
 	$('.start2').append(timeformat(leave_time2));
 	$('.start3').empty();
 	$('.start3').append(timeformat(leave_time3));
+	$('.end1').empty();
+	$('.end1').append(timeformat(arrive_time1));
+	$('.end2').empty();
+	$('.end2').append(timeformat(arrive_time2));
+	$('.end3').empty();
+	$('.end3').append(timeformat(arrive_time3));
+	$('.table').fadeIn("slow");
+};
+
+
+//-----------------------------------------------------------
+
+function render3(start_station,end_station,transfer_station,leave_time1,leave_time2,leave_time3,transfer_time1,transfer_time2,transfer_time3,arrive_time1,arrive_time2,arrive_time3){
+
+	$('.start_station').empty();
+	$('.start_station').append(start_station.name);
+	$('.transfer_station').remove();
+	$('.start_station').after('<td class="transfer_station">'+transfer_station+' (Transfer) </td>');
+	$('.end_station').empty();
+	$('.end_station').append(end_station.name);
+	$('.start1').empty();
+	$('.start1').append(timeformat(leave_time1));
+	$('.transfer1').remove();
+	$('.start1').after('<td class="transfer1">'+timeformat(transfer_time1)+'</td>');
+	$('.start2').empty();	
+	$('.start2').append(timeformat(leave_time2));
+	$('.transfer2').remove();
+	$('.start2').after('<td class="transfer2">'+timeformat(transfer_time2)+'</td>');
+	$('.start3').empty();
+	$('.start3').append(timeformat(leave_time3));
+	$('.transfer3').remove();
+	$('.start3').after('<td class="transfer3">'+timeformat(transfer_time3)+'</td>');
 	$('.end1').empty();
 	$('.end1').append(timeformat(arrive_time1));
 	$('.end2').empty();
