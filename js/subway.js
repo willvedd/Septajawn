a42fb243ac552022f81ae5f9a6354ff2d0da7b97
Will V <willvedd@gmail.com>
2014-03-28 00:23:13 -0400

    Uploaded vastly improved json file

--- js/subway.js
+++ js/subway.js
@@ -1,352 +1,21 @@
 
- //JSON TEST...and it works! need to swap out mess of objects below with somethign simpler via JSON
 
- $.getJSON('json/data.json', function(data) {
-        console.log(data.b_sat_nb[0].allegh);
-    });
 
- //Broad Street Line
+console.time('JSON function');
 
-var fern = {
-    line: "bs",
-    order: -14,
-    id: "fern",
-    name: "Fern Rock",
-};
-var olney = {
-    line: "bs",
-    order: -13,
-    id: "olney",
-    name: "Olney",
-};
-var logan = {
-    line: "bs",
-    order: -12,
-    id: "logan",
-    name: "Logan",
-};
-var wyoming = {
-    line: "bs",
-    order: -11,
-    id: "wyoming",
-    name: "Wyoming",
-};
-var hunt_park = {
-    line: "bs",
-    order: -10,
-    id: "hunt_park",
-    name: "Hunting Park",
-};
-var erie = {
-    line: "bs",
-    order: -9,
-    id: "erie",
-    name: "Erie",
-};
-var allegh = {
-    line: "bs",
-    order: -8,
-    id: "allegh",
-    name: "Allegheny",
-};
-var nphilly = {
-    line: "bs",
-    order: -7,
-    id: "nphilly",
-    name: "North Philly",
-};
-var susque = {
-    line: "bs",
-    order: -6,
-    id: "susque",
-    name: "Susquehanna-Dauphin",
-};
-var cecil = {
-    line: "bs",
-    order: -5,
-    id: "cecil",
-    name: "Cecil B Moore",
-};
-var girard = {
-    line: "bs",
-    order: -4,
-    id: "girard",
-    name: "Girard",
-};
-var fairmount = {
-    line: "bs",
-    order: -3,
-    id: "fairmount",
-    name: "Fairmount",
-    close_wk: 22.3,
-    close_end: 20.3,
-};
-var spring = {
-    line: "bs",
-    order: -2,
-    id: "spring",
-    name: "Spring Garden",
-};
-var race = {
-    line: "bs",
-    order: -1,
-    id: "race",
-    name: "Race-Vine",
-};
-var city_hall = {
-    line: "bs",
-    order: 0,
-    id: "city_hall",
-    name: "City Hall",
-};
-var walnut = {
-    line: "bs",
-    order: 1,
-    id: "walnut",
-    name: "Walnut-Locust",
-};
-var lombard = {
-    line: "bs",
-    order: 2,
-    id: "lombard",
-    name: "Lombard-South",
-};
-var ellsworth = {
-    line: "bs",
-    order: 3,
-    id: "ellsworth",
-    name: "Ellsworth-Federal",
-};
-var tasker = {
-    line: "bs",
-    order: 4,
-    id: "tasker",
-    name: "Tasker-Morris",
-};
-var snyder = {
-    line: "bs",
-    order: 5,
-    id: "snyder",
-    name: "Snyder",
-};
-var oregon = {
-    line: "bs",
-    order: 6,
-    id: "oregon",
-    name: "Oregon",
-};
-var att = {
-    line: "bs",
-    order: 7,
-    id: "att",
-    name: "AT&T",
-};
+$.getJSON('json/stations.json', function(data) {//importing JSON data
+    console.time('getJSON function');
+    window.stations = data;
+    
+    console.timeEnd('getJSON function');
 
-//Market Frankford Line
-var frank = {
-    line: "mf",
-    order: -17,
-    id: "frank",
-    name: "Frankford",
-};
-var marg = {
-    line: "mf",
-    order: -16,
-    id: "marg",
-    name: "Margaret-Orthodox",
-};
-var church = {
-    line: "mf",
-    order: -15,
-    id: "church",
-    name: "Church",
-};
-var eriet = {
-    line: "mf",
-    order: -14,
-    id: "eriet",
-    name: "Erie-Torresdale",
-};
-var tioga = {
-    line: "mf",
-    order: -13,
-    id: "tioga",
-    name: "Tioga",
-};
-var alleghmf = {
-    line: "mf",
-    order: -12,
-    id: "alleghmf",
-    name: "Allegheny",
-};
-var somer = {
-    line: "mf",
-    order: -11,
-    id: "somer",
-    name: "Somerset",
-};
-var hunt = {
-    line: "mf",
-    order: -10,
-    id: "hunt",
-    name: "Huntingdon",
-};
-var york = {
-    line: "mf",
-    order: -9,
-    id: "york",
-    name: "York-Dauphin",
-};
-var berks = {
-    line: "mf",
-    order: -8,
-    id: "berks",
-    name: "Berks",
-};
-var girardmf = {
-    line: "mf",
-    order: -7,
-    id: "girardmf",
-    name: "Girard",
-};
-var springmf = {
-    line: "mf",
-    order: -6,
-    id: "springmf",
-    name: "Spring Garden",
-};
-var second = {
-    line: "mf",
-    order: -5,
-    id: "second",
-    name: "2nd St",
-};
-var fifth = {
-    line: "mf",
-    order: -4,
-    id: "fifth",
-    name: "5th St",
-};
-var eight = {
-    line: "mf",
-    order: -3,
-    id: "eight",
-    name: "8th St",
-};
-var elev = {
-    line: "mf",
-    order: -2,
-    id: "elev",
-    name: "11th St",
-};
-var thirteen = {
-    line: "mf",
-    order: -1,
-    id: "thirteen",
-    name: "13th St",
-};
-var fif = {
-    line: "mf",
-    order: 0,
-    id: "fif",
-    name: "15th St",
-};
-var thirty = {
-    line: "mf",
-    order: 1,
-    id: "thirty",
-    name: "30th St",
- };
-var thirtyfour = {
-    line: "mf",
-    order: 2,
-    id: "thirtyfour",
-    name: "34th St",
-};
-var forty = {
-    line: "mf",
-    order: 3,
-    id: "forty",
-    name: "40th St",
-};
-var fortysix = {
-    line: "mf",
-    order: 4,
-    id: "fortysix",
-    name: "46th St",
-};
-var fiftytwo = {
-    line: "mf",
-    order: 5,
-    id: "fiftytwo",
-    name: "52nd St",
-};
-var fiftysix = {
-    line: "mf",
-    order: 6,
-    id: "fiftysix",
-    name: "52nd St",
-};
-var sixty = {
-    line: "mf",
-    order: 7,
-    id: "sixty",
-    name: "60th St",
-};
-var sixtythree = {
-    line: "mf",
-    order: 8,
-    id: "sixtythree",
-    name: "63rd St",
-};
-var milb = {
-    line: "mf",
-    order: 9,
-    id: "milb",
-    name: "Milbourne",
- };
-var sixtynine = {
-    line: "mf",
-    order: 10,
-    id: "sixtynine",
-    name: "69th St",
-};
+    console.time("Stations");
+    console.log(stations.city_hall.sat_nb);
+    console.log(stations.fern);
+    console.timeEnd("Stations");
+});
 
-//-----------------------------------------------------------
-
-var stations = [fern, olney, logan, wyoming, hunt_park, erie, allegh, nphilly, susque, cecil, girard, fairmount, spring, race, city_hall, walnut, lombard, ellsworth, tasker, snyder, oregon, att, frank, marg, church, eriet, tioga, alleghmf, somer, hunt, york, berks, girardmf, springmf, second, fifth, eight, elev, thirteen, fif, thirty, thirtyfour, forty, fortysix, fiftytwo, fiftysix, sixty, sixtythree, milb, sixtynine]; //Setting array of stations
-
-//-----------------------------------------------------------
-
-function schedule(prev_station, diff) { //sets schedules for all stations
-
-    var schedule = new Array();
-
-    if (diff > 0) { //for southbound and westbound schedules, positive difference
-        for (i = 0; i < prev_station.length; i++) {
-
-            schedule[i] = prev_station[i] + (diff / 100);
-
-            if ((schedule[i] - Math.floor(schedule[i])) > .59) {
-                var minutes = schedule[i] - Math.floor(schedule[i]) - .60;
-                schedule[i] = Math.floor(schedule[i]) + 1 + minutes;
-            }
-            schedule[i] = round(schedule[i]);
-        };
-    } else { //for northbound and eastbound schedules, negative difference
-        for (i = 0; i < prev_station.length; i++) {
-
-            schedule[i] = prev_station[i] + (diff / 100);
-
-            if ((schedule[i] - Math.floor(schedule[i])) > .59) {
-                var minutes = Math.ceil(schedule[i]) - schedule[i];
-                schedule[i] = Math.floor(schedule[i]) + (.6 - minutes);
-            }
-            schedule[i] = round(schedule[i]);
-        };
-    }
-    return schedule;
-};
+console.timeEnd('JSON function');
 
 //-----------------------------------------------------------
 
@@ -381,7 +50,7 @@ function submit() {
     
     var time = hours+"."+minutes;//setting time 
 
-    routeInit(start, end, time, day);
+    route(start, end, time, day);
 
     if((start&&end)!=undefined){
         $('#start_dest, #end_dest').change(function() { //After initial submission, script checks for changes and dynamically updates
@@ -391,7 +60,7 @@ function submit() {
             var end_sel = document.getElementById("end_dest");
             var end = end_sel.options[end_sel.selectedIndex].value;
 
-            routeInit(start, end, time, day); //Prevents console errors from undefined select variables
+            route(start, end, time, day); //Prevents console errors from undefined select variables
         });
     };
     
@@ -405,7 +74,7 @@ function submit() {
         window.day = "wk";
 
         if ((start_sel != undefined) && (end_sel != undefined)) {
-            routeInit(start, end, time, "wk"); //Prevents console errors from undefined select variables
+            route(start, end, time, "wk"); //Prevents console errors from undefined select variables
         }
     });
     $('#sat').click(function(){//dynamically modifies the results based on the day buttons
@@ -419,7 +88,7 @@ function submit() {
         window.day = "sat";
 
         if ((start_sel != undefined) && (end_sel != undefined)) {
-            routeInit(start, end, time, day); //Prevents console errors from undefined select variables
+            route(start, end, time, day); //Prevents console errors from undefined select variables
         }
 
     });
@@ -433,7 +102,7 @@ function submit() {
         window.day = "sun";
 
         if ((start_sel != undefined) && (end_sel != undefined)) {
-            routeInit(start, end, time, "sun");  //Prevents console errors from undefined select variables
+            route(start, end, time, "sun");  //Prevents console errors from undefined select variables
         }
     });  
     $('#rst').click(function(){//Resets the form and removes any previous results
@@ -463,7 +132,7 @@ function submit() {
 
 //-----------------------------------------------------------
 
-function routeInit(start, end, time, day) { //Using form to get times
+function routeInit(start, end, time, day) { //Passing form values to generate times
 
     console.log("Routeinit executed");
 
@@ -480,16 +149,14 @@ function routeInit(start, end, time, day) { //Using form to get times
 
 //-----------------------------------------------------------
 
-function route(start_station, end_station, day, time) {//Logic that generates requested times depending on direction and day
+function route(start_station, end_station, time, day) {//Logic that generates requested times depending on direction and day
 
     console.log("Route executed");
 
     var leave_time = new Array();
     var arrive_time = new Array();
     var i=0;
-    var sb;
-    var pointer;
-    var flag;
+    var sb, pointer, flag;
 
     if(start_station.order<end_station.order){
         sb = true;
@@ -512,26 +179,26 @@ function route(start_station, end_station, day, time) {//Logic that generates re
                 break;//need to stop the while loop
             };
             if(sb){
-                if((day=="wk")&&(start_station.sched_wk_sb[i]<time)){//compares schedule to current time, indicates which one is closest
+                if((day=="wk")&&(start_station.wk_sb[i]<time)){//compares schedule to current time, indicates which one is closest
                     pointer = i+1;//assigning pointer, incremented to get the next one
                     console.log("pointer funciton");
                 }
-                else if ((day=="sat")&&(start_station.sched_sat_sb[i]<time)){
+                else if ((day=="sat")&&(start_station.sat_sb[i]<time)){
                     pointer = i+1;//assigning pointer, incremented to get the next one
                 }
-                else if(start_station.sched_sun_sb[i]<time){
+                else if(start_station.sun_sb[i]<time){
                     pointer = i+1;//assigning pointer, incremented to get the next one
                 }
             }
             else{
-                if((day=="wk")&&(start_station.sched_wk_nb[i]<time)){//compares schedule to current time, indicates which one is closest
+                if((day=="wk")&&(start_station.wk_nb[i]<time)){//compares schedule to current time, indicates which one is closest
                     pointer = i+1;//assigning pointer, incremented to get the next one
                     console.log("pointer funciton");
                 }
-                else if ((day=="sat")&&(start_station.sched_sat_nb[i]<time)){
+                else if ((day=="sat")&&(start_station.sat_nb[i]<time)){
                     pointer = i+1;//assigning pointer, incremented to get the next one
                 }
-                else if(start_station.sched_sun_nb[i]<time){
+                else if(start_station.sun_nb[i]<time){
                     pointer = i+1;//assigning pointer, incremented to get the next one
                 }
             }
@@ -543,24 +210,24 @@ function route(start_station, end_station, day, time) {//Logic that generates re
 
     if (sb) { //southbound
         if (day === "wk") { //weekday southbound scheduling
-            subroute( start_station.sched_wk_sb , end_station.sched_wk_sb , start_station.close_wk );
+            subroute( start_station.wk_sb , end_station.wk_sb , start_station.close_wk );
         } 
         else if (day === "sat") { //saturday southbound scheduling
-            subroute( start_station.sched_sat_sb , end_station.sched_sat_sb , start_station.close_end );
+            subroute( start_station.sat_sb , end_station.sat_sb , start_station.close_end );
         }
         else { //sunday southbound scheduling
-            subroute( start_station.sched_sun_sb , end_station.sched_sun_sb , start_station.close_end );
+            subroute( start_station.sun_sb , end_station.sun_sb , start_station.close_end );
         };
     }
     else { //northbound
         if (day === "wk") { //weekday northbound scheduling
-            subroute( start_station.sched_wk_nb , end_station.sched_wk_nb , start_station.close_wk );
+            subroute( start_station.wk_nb , end_station.wk_nb , start_station.close_wk );
         } 
         else if (day === "sat") { //saturday northbound scheduling
-             subroute( start_station.sched_sat_nb , end_station.sched_sat_nb , start_station.close_end );
+             subroute( start_station.sat_nb , end_station.sat_nb , start_station.close_end );
         }
         else { //sunday northbound scheduling
-             subroute( start_station.sched_sun_nb , end_station.sched_sun_nb , start_station.close_end );
+             subroute( start_station.sun_nb , end_station.sun_nb , start_station.close_end );
         };
     };
 
@@ -705,10 +372,14 @@ $('.line1').ready(function() { //Function that populates starting station list a
 
     var lineval1 = $('.line1');
 
+    console.log("made it here5");
+
     $(".line1").change(function() {
         $('.submit').removeClass("hide");
         $('.table_and_tools').addClass('hide');
         $('.platter').addClass('vert_center');
+
+        console.log("made it here6");
 
         if (lineval1.is(":checked")) {
 
@@ -718,7 +389,7 @@ $('.line1').ready(function() { //Function that populates starting station list a
                 $('.label1').removeClass("bs");//changes color of switch button labels
                 $('.label2').addClass("mf");//changes color of switch button labels
             }
-
+            console.log("made it here7");
             $('.station').addClass("mf").removeClass("bs");//Changes color of table header
 
         } else {
@@ -732,6 +403,9 @@ $('.line1').ready(function() { //Function that populates starting station list a
             $('.station').addClass("bs").removeClass("mf");//Changes color of table header
         };
 
+        console.log("made it here8");
+
+
         $('#start_dest').empty();
         $('#end_dest').empty();
 
@@ -743,6 +417,7 @@ $('.line1').ready(function() { //Function that populates starting station list a
                 $('#start_dest').append("<option value='" + stations[i].id + "'>" + stations[i].name + "</option>");
             }
         }
+        console.log("made it here9");
     });
 });
 
@@ -750,15 +425,17 @@ $('.line1').ready(function() { //Function that populates starting station list a
 
 $('#start_dest').change(function() { //Function sees start destination and removes it as an option for end destination
 
-    console.log(dataz);
+    console.log("made it here1");
 
     var selection = document.getElementById("start_dest").options[document.getElementById("start_dest").selectedIndex].value;
 
     var selection_end = document.getElementById("end_dest").options[document.getElementById("end_dest").selectedIndex].value;
 
+    console.log("made it here2");
+
     if ((selection_end == "") || (selection == selection_end)) { //Resets drop down list if end isn't defined or if start and end are the s
         $('#end_dest').empty();
-
+        console.log("made it here3");
         $('#end_dest').append("<option value='' disabled='disabled' selected='selected'>Ending at...</option>");
 
 
@@ -775,6 +452,7 @@ $('#start_dest').change(function() { //Function sees start destination and remov
             };
         };
     };
+    console.log("made it here4");
 });
 
 //-----------------------------------------------------------
